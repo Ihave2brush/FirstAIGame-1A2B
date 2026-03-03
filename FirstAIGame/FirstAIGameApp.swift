b18300d317c59b2050abc98e8f84df360bee1378
Ihave2brush <ihave2brush@gmail.com>
2026-03-03 17:48:45 +0800

    Initial Commit

---
+++ FirstAIGame/FirstAIGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAIGameApp.swift
+//  FirstAIGame
+//
+//  Created by Kimi on 2026/3/3.
+//
+
+import SwiftUI
+
+@main
+struct FirstAIGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
